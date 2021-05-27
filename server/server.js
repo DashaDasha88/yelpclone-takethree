e39dcfe83e89dc87df8d306e661db296db29bb6f
Dasha <dasha.kotova.mail@gmail.com>
2021-05-27 19:16:28 -0400

    add restful api skeletons

--- server/server.js
+++ server/server.js
@@ -1,10 +1,56 @@
 require("dotenv").config();
 const express = require("express");
 
-const app = express()
+const app = express();
+
+//GET all restaurants
+app.get("api/v1/restaurants", async (req, res) => {
+  try {
+
+  } catch (err){
+    console.log(err);
+  }
+});
+
+//GET a restaurant
+app.get("api/v1/restaurants/:id", async (req, res) => {
+  try {
+
+  } catch (err){
+    console.log(err);
+  }
+
+});
+
+//CREATE a restaurant
+app.post("api/v1/restaurants", async (req, res) => {
+  try {
+
+  } catch (err){
+    console.log(err);
+  }
+});
+
+//UPDATE a restaurant
+app.put("api/v1/restaurants/:id", async (req, res) => {
+  try {
+
+  } catch (err){
+    console.log(err);
+  }
+});
+
+//DELETE a restaurant
+app.delete("/api/v1/restaurants", async (req, res) => {
+  try {
+
+  } catch (err){
+    console.log(err);
+  }
+
+});
 
 const port = process.env.PORT || 3001;
-
 app.listen(port, () => {
   console.log(`o hi - server is up and listening on port ${port}`);
 });
