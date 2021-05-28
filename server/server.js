3ed2d15dc558a6e7373305aa56dcb5076e0fb175
Dasha <dasha.kotova.mail@gmail.com>
2021-05-28 13:02:58 -0400

    fix an issue with server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const db = require("./db");
 
-const app = express("dev");
+const app = express();
 
 app.use(express.json());
 
